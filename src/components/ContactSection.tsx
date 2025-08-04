3452daaf755d2664d2cd6ccf4a6c70095de51736
Vithuran Jeyasingham <vithuranj@gmail.com>
2025-08-04 21:04:55 +1000

    adjust form styling

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -4,6 +4,16 @@ const ContactSection: React.FC = () => (
   <section id="contact" className="py-20 bg-pink-100">
     <div className="container mx-auto px-4 max-w-lg">
       <h3 className="text-3xl font-semibold text-center mb-8">Contact Us</h3>
+      
+      <div className="mb-4 max-w-lg" style={{ height: '1000px' }}>
+        <iframe 
+          src='https://articulate-allied-health.splose.com/public-form/747b15b1-827b-4164-8424-2457c1edf104'
+          name='embeddableForm'
+          className="w-full h-full" 
+          loading='lazy'
+        />
+      </div>
+      
       <p className="text-center mb-4">
         Articulate Allied Health - ABN: 52677440729<br />
         PO Box 123, South Yarra, Melbourne VIC Australia
@@ -12,15 +22,6 @@ const ContactSection: React.FC = () => (
         Email: <a href="mailto:info@articulateah.com.au" className="text-blue-600 hover:underline">info@articulateah.com.au</a><br />
         Phone: <a href="tel:+61398123456" className="text-blue-600 hover:underline">+61 3 9812 3456</a>
       </p>
-      <div className="relative max-w-lg h-screen">
-        <iframe 
-          src='https://articulate-allied-health.splose.com/public-form/747b15b1-827b-4164-8424-2457c1edf104'
-          name='embeddableForm'
-          className="absolute top-0 left-0 w-full h-full"
-          loading='lazy'
-          height={'100%'}
-        />
-      </div>
     </div>
   </section>
 );
